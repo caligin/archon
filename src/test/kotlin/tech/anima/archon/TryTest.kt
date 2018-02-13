0bc3aa70f60a31673c10d42750b4fd6c2ccfce35
Tsukihara Caligin <caligin35@gmail.com>
2018-02-13 01:03:23 +0000

    enh: javacc setup and sample grammar

--- src/test/kotlin/tech/anima/archon/TryTest.kt
+++ src/test/kotlin/tech/anima/archon/TryTest.kt
@@ -2,11 +2,12 @@ package tech.anima.archon
 
 import kotlin.test.assertEquals
 import org.junit.Test
+import tech.anima.archon.language.Archon
 
 public class TryTest {
 
 	@Test fun pass() {
-		assertEquals("asd", "asd")
+		assertEquals("cid", Archon.Factory().make("correlate cid").emit())
 	}
 
 }
